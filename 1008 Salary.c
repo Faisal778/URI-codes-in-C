f146e5d632971a8fd9f8a47af587bbf6670a47eb
Faisal Mahmud <62201274+Faisal778@users.noreply.github.com>
2021-08-30 00:07:04 +0600

    Update and rename 1008 Salary to 1008 Salary.c

--- 1008 Salary.c
+++ 1008 Salary.c
@@ -4,12 +4,8 @@ int main()
 {
     int a,b;
     float c;
-    
-    
+     
     scanf("%d %d %f", &a, &b, &c);
-    
-    
-    
     printf("NUMBER = %d\n",a);
     printf("SALARY = U$ %.2f\n",b*c);
 
